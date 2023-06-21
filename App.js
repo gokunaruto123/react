90acbdaf587389bbd3955a0eb47043962248d21b
theo <84184692+gokunaruto123@users.noreply.github.com>
2023-06-21 12:35:12 -0300

    atividade react

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import "./style.css";
-import Header from "./Header.js";
-import Main from "./main.js";
-import Footer from "./Footer.js";
+import styled from "styled-components";
+import Header from "./comp/Header/Header";
+import Footer from "./comp/Footer/Footer.js";
+import Home from "./comp/Home/Home.js";
+import Nav from "./comp/Nav/Nav.js"
+import { TelaInteira } from "./styledglobal";
 function App() {
+  function reproduzVideo() {
+    alert("O vídeo está sendo reproduzido")
+}
   return (
-<body>
-   <div class="tela-inteira">
+  
+  <TelaInteira>
     <Header/>
-    <Main/>
+    <Nav/>
+    <Home reproduz={reproduzVideo} />
     <Footer/>
-  </div>
-</body>
+  </TelaInteira>
   );
 }
 
